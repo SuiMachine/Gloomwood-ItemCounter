845bdfa0211506fd2bb1ecdd3834aac9978bbee3
SuiMachine <SuiMachine@users.noreply.github.com>
2022-09-08 21:11:02 +0200

    Ignore files etc.

---
+++ Source/ItemCounter.cs
@@ -0,0 +1,81 @@
+﻿using Gloomwood.Entity.Items;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ItemCounter
+{
+	public class ItemCounter : MonoBehaviour
+	{
+		private static ItemCounter instance;
+		List<ItemPickup> treasure = new List<ItemPickup>();
+		List<ItemPickup> currency = new List<ItemPickup>();
+
+
+		public static void Initialize()
+		{
+			if(instance == null)
+			{
+				instance = new GameObject("Item Counter").AddComponent<ItemCounter>();
+				DontDestroyOnLoad(instance.gameObject);
+				Plugin.Msg("Initialized item counter object!");
+			}
+		}
+
+		public static void Reinitialize()
+		{
+			if(instance != null)
+			{
+				instance.ReinistializeInstance();
+			}
+		}
+
+		private void ReinistializeInstance()
+		{
+			StopAllCoroutines();
+			StartCoroutine(Recount());
+		}
+
+		private IEnumerator Recount()
+		{
+			yield return null;
+			treasure = FindObjectsOfType<ItemPickup>().Where(x => x.isActiveAndEnabled && x.EntityType == Gloomwood.Entity.EntityTypes.Pickups).Where(x => x.Config != null && x.Config.ItemType.ItemGroup == ItemGroups.Treasure).ToList();
+			currency = FindObjectsOfType<ItemPickup>().Where(x => x.isActiveAndEnabled && x.EntityType == Gloomwood.Entity.EntityTypes.Pickups).Where(x => x.Config != null && x.Config.ItemType.ItemGroup == ItemGroups.Currency).ToList();
+
+			yield return null;
+
+			while (true)
+			{
+				for(int i = treasure.Count -1; i>=0 && i < treasure.Count; i--)
+				{
+					if(treasure[i] == null || !treasure[i].isActiveAndEnabled)
+					{
+						treasure.RemoveAt(i);
+					}
+					yield return null;
+				}
+
+				for (int i = currency.Count - 1; i >= 0 && i < currency.Count; i--)
+				{
+					if (currency[i] == null || !currency[i].isActiveAndEnabled)
+					{
+						currency.RemoveAt(i);
+					}
+					yield return null;
+				}
+				yield return null;
+			}
+		}
+
+		void OnGUI()
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.BeginVertical(GUI.skin.box);
+			GUILayout.Label("Treasure left:" + treasure.Count.ToString());
+			GUILayout.Label("Currency left:" + currency.Count.ToString());
+			GUILayout.EndVertical();
+			GUILayout.EndHorizontal();
+		}
+	}
+}
---
+++ Source/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+
+namespace ItemCounter
+{
+	public class Plugin : MelonMod
+    {
+		private static Plugin instance;
+
+		public override void OnApplicationStart()
+		{
+			instance = this;
+			LoggerInstance.Msg("Initializing this thing");
+			base.OnApplicationStart();
+		}
+
+		public override void OnApplicationLateStart()
+		{
+			base.OnApplicationLateStart();
+			ItemCounter.Initialize();
+		}
+
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+		{
+			base.OnSceneWasLoaded(buildIndex, sceneName);
+			ItemCounter.Reinitialize();
+		}
+
+		public static void Msg(object obj)
+		{
+			instance.LoggerInstance.Msg(obj);
+		}
+
+		public static void Error(object obj)
+		{
+			instance.LoggerInstance.Error(obj);
+		}
+	}
+}
